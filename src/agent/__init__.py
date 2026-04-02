75e7f81efe94dc0ea03d6f66a3045902d3ea2f18
yoshiioko <acardoza@pdx.edu>
2026-04-02 12:10:40 -0700

    Update missing graph/runner exports.

--- src/agent/__init__.py
+++ src/agent/__init__.py
@@ -1,8 +1,11 @@
 """Agent package exports."""
 
 from .config import Settings, load_settings
+from .graph import build_graph, route_after_critic
+from .runner import run_cli, run_once
 from .schemas import CompilerOutput, CriticAssessment
 from .state import AgentState
+
 
 __all__ = [
     "Settings",
@@ -10,4 +13,8 @@ __all__ = [
     "CriticAssessment",
     "CompilerOutput",
     "AgentState",
+    "build_graph",
+    "route_after_critic",
+    "run_once",
+    "run_cli",
 ]
