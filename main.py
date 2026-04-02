eadba0144c1f917d9e511cbd12c2bb62e330d10d
yoshiioko <acardoza@pdx.edu>
2026-04-02 12:09:15 -0700

    Update main.py to use run_cli().

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
-# This is a sample Python script.
+"""Project entrypoint."""
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+from __future__ import annotations
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+from src.agent.runner import run_cli
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if __name__ == "__main__":
+    run_cli()
